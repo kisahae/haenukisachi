c3a236651a00376942b69a38337ad32a64065081
Haenuki Sachi <sachihaenuki1@gmail.com>
2023-03-07 15:01:06 +0700

    Revise the content

--- src/components/VerticalTimeline/verticaltimeline.js
+++ src/components/VerticalTimeline/verticaltimeline.js
@@ -1,58 +1,67 @@
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
-import 'react-vertical-timeline-component/style.min.css';
-import { HiUsers,HiOutlineCode } from "react-icons/hi"
-import { IoIosNuclear } from "react-icons/io";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement
+} from "react-vertical-timeline-component"
+import "react-vertical-timeline-component/style.min.css"
+import { HiUsers, HiOutlineCode } from "react-icons/hi"
+import { IoIosNuclear } from "react-icons/io"
 import "./verticaltimeline.css"
 
-function Timeline(){
+function Timeline() {
   const educationData = [
     {
-      date:"November 2022 - February 2023",
-      icon:<HiOutlineCode />,
-      title:"Software Developer Intern",
-      subtitle:"Serpong, Tangerang Selatan",
-      explanation:"React JS, Storybook, Babel, Rollup, Tailwind CSS, Bitbucket, and Jira."
+      date: "November 2022 - February 2023",
+      icon: <HiOutlineCode />,
+      title: "Software Developer Intern",
+      subtitle: "Serpong, Tangerang Selatan",
+      explanation:
+        "Did some researches and maintained the web system using React JS, Storybook, Babel, Rollup, Tailwind CSS, Bitbucket, and Jira."
     },
     {
-      date:"September 2021 - October 2021",
-      icon:<IoIosNuclear />,
-      title:"Instrumentation and Control Intern",
-      subtitle:"Serpong, Tangerang Selatan",
-      explanation:"Creating the simulation of fire alarm system for nuclear reactor using LabVIEW and Proteus."
+      date: "June 2019 - August 2022",
+      icon: <HiUsers />,
+      title: "Computer Lab Assistant",
+      subtitle: "Serpong, Tangerang Selatan",
+      explanation: "Maintaining the lab facilities and assisting the lecturer."
     },
     {
-      date:"June 2019 - August 2022",
-      icon:<HiUsers />,
-      title:"Computer Lab Assistant",
-      subtitle:"Serpong, Tangerang Selatan",
-      explanation:"Maintaining the lab facilities and assisting the lecturer."
+      date: "September 2021 - October 2021",
+      icon: <IoIosNuclear />,
+      title: "Instrumentation and Control Intern",
+      subtitle: "Serpong, Tangerang Selatan",
+      explanation:
+        "Creating the simulation of fire alarm system for nuclear reactor using C++, LabVIEW, and Proteus."
     }
   ]
-    return (
-      <div>
-          <div id="experience">
-            <VerticalTimeline lineColor="#000">
-            {
-                educationData.map(function (value){
-                  return <VerticalTimelineElement 
-                          className="vertical-timeline-element--work transition-all duration-200 hover:scale-105"
-                          contentArrowStyle={{ borderRight: '7px solid  rgb(25, 26, 28)' }}
-                          date={value.date}
-                          iconStyle={{ background: 'rgb(25, 26, 28)', color: '#fff' }}
-                          icon={value.icon}
-                        >
-                          <h3 className="vertical-timeline-element-title">{value.title}</h3>
-                          <h4 className="vertical-timeline-element-subtitle">{value.subtitle}</h4>
-                          <p>
-                              {value.explanation}
-                          </p>
-                        </VerticalTimelineElement>
-                  })
-            }
-            </VerticalTimeline>
-          </div>
+  return (
+    <div>
+      <div id="experience">
+        <VerticalTimeline lineColor="#000">
+          {educationData.map(function (value) {
+            return (
+              <VerticalTimelineElement
+                className="vertical-timeline-element--work transition-all duration-200 hover:scale-105"
+                contentArrowStyle={{
+                  borderRight: "7px solid  rgb(25, 26, 28)"
+                }}
+                date={value.date}
+                iconStyle={{ background: "rgb(25, 26, 28)", color: "#fff" }}
+                icon={value.icon}
+              >
+                <h3 className="vertical-timeline-element-title">
+                  {value.title}
+                </h3>
+                <h4 className="vertical-timeline-element-subtitle">
+                  {value.subtitle}
+                </h4>
+                <p>{value.explanation}</p>
+              </VerticalTimelineElement>
+            )
+          })}
+        </VerticalTimeline>
       </div>
-)
+    </div>
+  )
 }
 
 export default Timeline
--- src/components/verticaltimelineeducation/verticaltimelineedu.js
+++ src/components/verticaltimelineeducation/verticaltimelineedu.js
@@ -1,62 +1,77 @@
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
-import 'react-vertical-timeline-component/style.min.css';
+import {
+  VerticalTimeline,
+  VerticalTimelineElement
+} from "react-vertical-timeline-component"
+import "react-vertical-timeline-component/style.min.css"
 import Hanyang_univ from "../../images/hanyang_univ.png"
 import asia_univ from "../../images/asia_univ.jpg"
 import atmalogo from "../../images/atma-univlogo.png"
 
-function VerticalEducationTimeline(){
+function VerticalEducationTimeline() {
   const education = [
     {
       img: atmalogo,
       alt: "Atma Jaya Logo",
-      date:"August 2018- Jan 2023",
-      title:"Atma Jaya Catholic University",
-      subtitle:"Serpong, Tangerang Selatan",
-      description:"Electrical Engineering Department (GPA: 3.99/4.00), Activities: Atma Jaya Wushu Club, OASE, and Atma Jaya Artificial Intelligence Base Club (AJAIB).",
-      height:"150%"
+      date: "August 2018- Jan 2023",
+      title: "Atma Jaya Catholic University",
+      subtitle: "Serpong, Tangerang Selatan",
+      description:
+        "Electrical Engineering Department (GPA: 3.99/4.00), Activities: Atma Jaya Wushu Club, OASE, and Atma Jaya Artificial Intelligence Base Club (AJAIB).",
+      height: "150%"
     },
     {
       img: Hanyang_univ,
       alt: "Hanyang Logo",
-      date:"March 2022 - June 2022",
-      title:"Hanyang University",
-      subtitle:"Ansan, South Korea",
-      description:"Robot Engineering (GPA:4.18/4.5), Course Taken: ICT Fusion Robotics, Game Programming Language, Japan and Global Community, Advanced English Writing, and Survival Korean Class.",
-      height:"100%"
+      date: "March 2022 - June 2022",
+      title: "Hanyang University",
+      subtitle: "Ansan, South Korea",
+      description:
+        "Robot Engineering (GPA:4.18/4.5), Course Taken: ICT Fusion Robotics, Game Programming Language, Japan and Global Community, Advanced English Writing, and Survival Korean Class.",
+      height: "100%"
     },
     {
       img: asia_univ,
       alt: "Asia Logo",
-      date:"September 2021 - January 2022",
-      title:"Asia University Taiwan",
-      subtitle:"Taichung, Taiwan",
-      description:"Virtual Exchange Student, Course Taken: Digital Image Processing and Data Science.",
-      height:"100%"
+      date: "September 2021 - January 2022",
+      title: "Asia University Taiwan",
+      subtitle: "Taichung, Taiwan",
+      description:
+        "Virtual Exchange Student, Course Taken: Digital Image Processing (96.2) and Data Science (98).",
+      height: "100%"
     }
   ]
-    return(
-        <div id = "education">
-            <VerticalTimeline lineColor="#000">
-              {
-                education.map(function (value){
-                  return <VerticalTimelineElement
-                  className="vertical-timeline-element--work transition-all duration-200 hover:scale-105"
-                  contentArrowStyle={{ borderRight: '7px solid  rgb(25, 26, 28)' }}
-                  icon={<img src={value.img} alt={value.alt} style={{backgroundColor:"white",height:`${value.height}`,margin:"auto"}}></img>}
-                  date={value.date}
-                >
-                  <h3 className="vertical-timeline-element-title">{value.title}</h3>
-                  <h4 className="vertical-timeline-element-subtitle">{value.subtitle}</h4>
-                  <p>
-                    {value.description}
-                  </p>
-                </VerticalTimelineElement>
-                })
+  return (
+    <div id="education">
+      <VerticalTimeline lineColor="#000">
+        {education.map(function (value) {
+          return (
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work transition-all duration-200 hover:scale-105"
+              contentArrowStyle={{ borderRight: "7px solid  rgb(25, 26, 28)" }}
+              icon={
+                <img
+                  src={value.img}
+                  alt={value.alt}
+                  style={{
+                    backgroundColor: "white",
+                    height: `${value.height}`,
+                    margin: "auto"
+                  }}
+                ></img>
               }
-            </VerticalTimeline>
-  </div>
-    )
+              date={value.date}
+            >
+              <h3 className="vertical-timeline-element-title">{value.title}</h3>
+              <h4 className="vertical-timeline-element-subtitle">
+                {value.subtitle}
+              </h4>
+              <p>{value.description}</p>
+            </VerticalTimelineElement>
+          )
+        })}
+      </VerticalTimeline>
+    </div>
+  )
 }
 
-
-  export default VerticalEducationTimeline
+export default VerticalEducationTimeline
